1764a72b47eab1f8769112a1c47214d6dbae205b
MdHasib01 <md.hasibuzzaman001@gmail.com>
2021-12-18 23:34:46 +0600

    last commit

--- src/App.js
+++ src/App.js
@@ -7,32 +7,29 @@ import LatestNews from "./pages/LatestNews/LatestNews";
 import SingleNews from "./pages/SingleNews/SingleNews";
 import NewsByCatagory from "./pages/NewsByCatagory/NewsByCatagory";
 import AddNews from "./pages/Dashboard/AddNews/AddNews";
-import AuthProvider from "./contexts/AuthProvider/AuthProvider";
-import PrivateRoute from "./pages/Login/PrivateRoute/PrivateRoute";
-import Login from "./pages/Login/Login/Login";
+// import AuthProvider from "./contexts/AuthProvider/AuthProvider";
+// import PrivateRoute from "./pages/Login/PrivateRoute/PrivateRoute";
+// import Login from "./pages/Login/Login/Login";
 
 function App() {
   return (
     <div className="App">
-      <AuthProvider>
-        <BrowserRouter>
-          <NavigationBar></NavigationBar>
-          <Routes>
-            <Route path="/" element={<Home></Home>} />
-            <Route path="/latest_news" element={<LatestNews></LatestNews>} />
-            <Route path="/latest_news/:id" element={<SingleNews />} />
-            <Route path="/category" element={<NewsByCatagory />} />
-            <Route
-              path="/category/:categoryName"
-              element={<NewsByCatagory />}
-            />
+      {/* <AuthProvider> */}
+      <BrowserRouter>
+        <NavigationBar></NavigationBar>
+        <Routes>
+          <Route path="/" element={<Home></Home>} />
+          <Route path="/latest_news" element={<LatestNews></LatestNews>} />
+          <Route path="/latest_news/:id" element={<SingleNews />} />
+          <Route path="/category" element={<NewsByCatagory />} />
+          <Route path="/category/:categoryName" element={<NewsByCatagory />} />
 
-            <Route path="/login" element={<Login />} />
+          {/* <Route path="/login" element={<Login />} /> */}
 
-            <Route path="/add_news" element={<AddNews />} />
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+          <Route path="/add_news" element={<AddNews />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </AuthProvider> */}
     </div>
   );
 }
